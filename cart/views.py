c12affe2f9cca80e3d7edd7989fbb614d5caf1af
unknown <avneeshgarimella@gmail.com>
2019-09-24 01:46:00 +0530

    Haha fucked it up

--- cart/views.py
+++ cart/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def cart(request):
-	return render(request,'cart.html')
+    return render(request, 'cart342352352342525.html')
