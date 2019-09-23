6102ef67ec1d7126189aacbb19f14c34474067a5
Tanvi Vishwanath <tanvivishwanath@Tanvis-MacBook-Air-4.local>
2019-09-24 01:39:16 +0530

    adds cart page

--- cart/views.py
+++ cart/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+def cart(request):
+	return render(request,'cart.html')
