273d72ac42c97a06f10a1140b15ad51d545656b8
unknown <avneeshgarimella@gmail.com>
2019-10-15 00:12:12 +0530

    home page

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart, name='cart'),
+    path('', views.index, name='index'),
+    path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,44 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import product, transaction,order
+from .models import product, transaction, order
 from django.template import loader
 
 from .forms import PaymentInfo
 
+
 def detail(request, product_id):
-    products = product.objects.all().filter(product_ID = product_id)[0]
+    products = product.objects.all().filter(product_ID=product_id)[0]
     template = loader.get_template('cart/templates/detail.html')
     context = {
         'products': products,
     }
     return HttpResponse(template.render(context, request))
-    
+
+
 def index(request):
     products = product.objects.all()
-    template =  loader.get_template('cart/index.html')
+    template = loader.get_template('index.html')
     context = {
         'products': products,
     }
     return HttpResponse(template.render(context, request))
 
+
 def equipment(request):
-    products = product.objects.all().filter(category = "Equipment")
+    products = product.objects.all().filter(category="Equipment")
     template = loader.get_template('cart/equipment.html')
     context = {
         'products': products,
     }
     return HttpResponse(template.render(context, request))
 
+
 def shoes(request):
-    products = product.objects.all().filter(category = "Shoes")
+    products = product.objects.all().filter(category="Shoes")
     template = loader.get_template('cart/templates/shoes.html')
     context = {
         'products': products,
     }
     return HttpResponse(template.render(context, request))
 
+
 def clothing(request):
-    products = product.objects.all().filter(category = "Clothing")
+    products = product.objects.all().filter(category="Clothing")
     template = loader.get_template('ecommerce/fashion.html')
     context = {
         'products': products,
@@ -47,33 +52,35 @@ def clothing(request):
 
 
 def createpost(request, product_id):
-        products = product.objects.get(product_ID = product_id)
-        products.cart = "1"
-        products.quantity = products.quantity + 1
-        products.save(update_fields=['cart'])
-        products.save(update_fields=['quantity'])
-        template = loader.get_template('cart/added_cart.html')
-        context = {
-            'products': products,
-        }
-        return HttpResponse(template.render(context, request))
+    products = product.objects.get(product_ID=product_id)
+    products.cart = "1"
+    products.quantity = products.quantity + 1
+    products.save(update_fields=['cart'])
+    products.save(update_fields=['quantity'])
+    template = loader.get_template('cart/added_cart.html')
+    context = {
+        'products': products,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def cart(request):
-    products = product.objects.all().filter(cart = "1")
+    products = product.objects.all().filter(cart="1")
     template = loader.get_template('cart.html')
     context = {
         'products': products,
     }
     return HttpResponse(template.render(context, request))
 
+
 def checkout(request):
-    products = product.objects.all().filter(cart = "1")
+    products = product.objects.all().filter(cart="1")
     context = {
         'products': products,
-    } 
+    }
     template = loader.get_template('checkout.html')
     return HttpResponse(template.render(context, request))
+
 
 def payment(request):
     template = loader.get_template('cart/payment.html')
@@ -81,21 +88,21 @@ def payment(request):
         form = PaymentInfo()
         context = {'form': form}
         return HttpResponse(template.render(context, request))
-    
+
     else:
         form = PaymentInfo(request.POST)
         if form.is_valid():
-            products = product.objects.all().filter(cart = "1")
+            products = product.objects.all().filter(cart="1")
             ID = len(transaction.objects.all()) + 1
             for p in products:
-                t = transaction(transaction_ID = ID, product_ID = p, quantity = p.quantity)
+                t = transaction(transaction_ID=ID, product_ID=p, quantity=p.quantity)
                 t.save()
-                s = shipping(transaction_ID = t)
+                s = shipping(transaction_ID=t)
                 s.save()
                 p.cart = "0"
                 p.product_stock = int(p.product_stock) - int(p.quantity)
                 p.quantity = 0
-                p.save(update_fields = ['product_stock'])
+                p.save(update_fields=['product_stock'])
                 p.save(update_fields=['cart'])
                 p.save(update_fields=['quantity'])
             instance = form.save(commit=False)
@@ -103,22 +110,24 @@ def payment(request):
             instance.save()
         template = loader.get_template('cart/complete.html')
         return HttpResponse(template.render({}, request))
-    
+
+
 def completepayment(request):
     template = loader.get_template('cart/complete.html')
     return HttpResponse(template.render({}, request))
 
-def search(request):     
-    if(request.method == 'POST'):      
-        search = request.POST.get('search')      
-        status = product.objects.all().filter(product_name = search)
+
+def search(request):
+    if(request.method == 'POST'):
+        search = request.POST.get('search')
+        status = product.objects.all().filter(product_name=search)
         context = {
-            "product":product
+            "product": product
         }
         template = loader.get_template('cart/templates/search.html')
         return template.render(context, request)
     else:
-        return render(request,"search.html",{})
+        return render(request, "search.html", {})
 
 # Create your views here.
 
