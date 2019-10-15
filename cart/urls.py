405b70368cd574a9e09e362aca990ac4b5eeb6ee
unknown <avneeshgarimella@gmail.com>
2019-10-15 22:23:40 +0530

    Dummy commit

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cart/', views.cart, name='cart'),
-    path('checkout/', views.checkout, name='checkout')
+    path('checkout/', views.checkout, name='checkout'),
+    path('<int:product_id>/POST', views.createpost, name='createpost'),
 ]
